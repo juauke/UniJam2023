693ceea71303a6d3acc69ebc6e47a90df19105d6
Pierre Curallucci de Peretti <piercurapro@hotmail.com>
2023-11-25 15:40:00 +0100

    Initialization of TypeTile

--- Assets/Scripts/TileMapManager.cs
+++ Assets/Scripts/TileMapManager.cs
@@ -23,7 +23,10 @@ public class TileMapManager : MonoBehaviour
         public Sprite sprite;
     }
     [SerializeField]
-    private Sprite[][] _sprite;
+    private Sprite[] _spriteFloor;
+
+    [SerializeField]
+    private Sprite[] _spriteWater;
 
     [SerializeField]
     public Tilemap tileMap;
@@ -34,17 +37,48 @@ public class TileMapManager : MonoBehaviour
     [SerializeField]
     public Vector2Int bottomLeft;
 
-    [SerializeField] public Data_Tile[][] _data;
+    [SerializeField] public Data_Tile[,] _data=new Data_Tile[50,50];
 
     public void Awake()
     {
-        var size = bottomLeft - topRight;
-        for (int i = 0; i < size.x; i++)
+        var size = topRight - bottomLeft;
+        for (int i = 0; i <= size.x; i++)
         {
-            for (int j = 0; j < size.y; j++)
+            for (int j = 0; j <= size.y; j++)
             {
-                _data[j][i].tile = tileMap.GetTile<Tile>(new Vector3Int(bottomLeft.x + i, topRight.y - j,0));
-                _data[j][i].sprite = _data[j][i].tile.GetComponent<Sprite>();
+                _data[j,i]=new Data_Tile();
+                _data[j,i].tile = 
+                    tileMap.GetTile<Tile>(
+                    new Vector3Int(
+                        bottomLeft.x + i, 
+                        topRight.y - j,
+                        0)
+                    );
+                _data[j,i].sprite = _data[j,i].tile.sprite;
+                switch (_data[j,i].sprite.name)
+                { 
+                    case "casebase" :
+                        _data[j,i].type = TypeTile.Floor;
+                        break;
+                    case "casefeu":
+                        _data[j,i].type = TypeTile.Floor;
+                        break;
+                    case "caseglace":
+                        _data[j,i].type = TypeTile.Floor;
+                        break;
+                    case "murbase":
+                        _data[j,i].type = TypeTile.Wall;
+                        break;
+                    case "eaubase":
+                        _data[j,i].type = TypeTile.Water;
+                        break;
+                    case "eaugelée":
+                        _data[j,i].type = TypeTile.Water;
+                        break;
+                    default:
+                        break;
+
+                }
             }
         }
 
@@ -52,12 +86,23 @@ public class TileMapManager : MonoBehaviour
 
     public void UpdateTile(Data_Tile tile)
     {
-        tile.sprite = _sprite[(int)tile.type][tile.temperature];
+        switch (tile.type)
+        {
+            case TypeTile.Floor:
+                tile.sprite = _spriteFloor[tile.temperature];
+                break;
+            case TypeTile.Water:
+                if (tile.temperature == 0)
+                    tile.sprite = _spriteFloor[0];
+                else
+                    tile.sprite = _spriteFloor[1];
+                break;
+        }
     }
 
     public Data_Tile getData(Tile tile)
     {
         Vector3 tilePosition = tile.transform.GetPosition();
-        return _data[(int)tilePosition.x][(int)tilePosition.y];
+        return _data[(int)tilePosition.x,(int)tilePosition.y];
     }
 }
