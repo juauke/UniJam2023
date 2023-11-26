31649c36705975008e6a477219d89e03482bffb5
juauke <juauke@live.fr>
2023-11-26 09:29:09 +0100

    Fixed raycast for picking up crystals (now properly picks up crystals) and improved input system by using sliders between -1 and 1 for directional movement.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,104 +1,125 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     // Start is called before the first frame update
-    [SerializeField]
-    private int speed;
+    [SerializeField] private int speed;
     [SerializeField] private float sqrEpsilon = 1;
+    [SerializeField] private Transform grabRayStart;
 
-    public Cristal cristal= null;
+
+    [FormerlySerializedAs("cristal")] public Crystal crystal = null;
 
     private Rigidbody2D rb;
 
-    [SerializeField]
-    private GameObject pauseMenuCanvas;
+    [SerializeField] private GameObject pauseMenuCanvas;
 
     private Vector2 lookingDirection;
-    
+
     private bool isSliding = false;
 
-    [SerializeField]
-    private PlayerAudioManager playerAudioManager;
+    [SerializeField] private PlayerAudioManager playerAudioManager;
 
     [SerializeField] private Sprite[] sprites;
     private SpriteRenderer _spriteRenderer;
 
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
             pauseMenuCanvas.SetActive(!pauseMenuCanvas.activeSelf);
             playerAudioManager.PlaySelectSound();
         }
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            if (crystal) {
+                if (crystal.Place(transform.position + (Vector3)lookingDirection)) {
+                    crystal = null;
+                    playerAudioManager.PlayDropSound();
+                }
+            }
+            else {
+                var colliderhit = Physics2D.Raycast(grabRayStart.position, lookingDirection, sqrEpsilon,
+                    LayerMask.GetMask("Crystal")).collider;
+                if (colliderhit && colliderhit.gameObject.TryGetComponent<Crystal>(out crystal)) {
+                    crystal.Pick();
+                    playerAudioManager.PlayTakeSound();
+                }
+            }
+        }
     }
 
-    void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
-        {
-            rb.MovePosition(new Vector3(0, 1 * speed * Time.deltaTime, 0) + transform.position);
-            lookingDirection = new Vector2 (0, 1);
+    void FixedUpdate() {
+        int horizontalMovement = 0;
+        int verticalMovement = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W)) {
+            verticalMovement++;
+
+            lookingDirection = new Vector2(0, 1);
             playerAudioManager.PlayStepSound();
-            _spriteRenderer.sprite =sprites[0];
+            _spriteRenderer.sprite = sprites[0];
             _spriteRenderer.flipX = false;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-        {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            verticalMovement--;
             rb.MovePosition(new Vector3(0, -1 * speed * Time.deltaTime, 0) + transform.position);
-            lookingDirection =  new Vector2(0, -1);
+            lookingDirection = new Vector2(0, -1);
             playerAudioManager.PlayStepSound();
             _spriteRenderer.sprite = sprites[1];
             _spriteRenderer.flipX = false;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.A))
-        {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.A)) {
+            horizontalMovement--;
             rb.MovePosition(new Vector3(-1 * speed * Time.deltaTime, 0, 0) + transform.position);
-            lookingDirection =  new Vector2(-1, 0);
+            lookingDirection = new Vector2(-1, 0);
             playerAudioManager.PlayStepSound();
             _spriteRenderer.sprite = sprites[2];
             _spriteRenderer.flipX = true;
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            horizontalMovement++;
             rb.MovePosition(new Vector3(1 * speed * Time.deltaTime, 0, 0) + transform.position);
-            lookingDirection =  new Vector2(1, 0);
+            lookingDirection = new Vector2(1, 0);
             playerAudioManager.PlayStepSound();
             _spriteRenderer.sprite = sprites[2];
             _spriteRenderer.flipX = false;
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if (cristal)
-            {
-                cristal.Place(transform.position);
-                cristal = null;
-                playerAudioManager.PlayDropSound();
-            }
-            else
-            {
-                var colliderhit = Physics2D.Raycast(transform.position, lookingDirection, sqrEpsilon).collider;
-                if (colliderhit && colliderhit.gameObject.TryGetComponent<Cristal>(out cristal))
-                {
-                    cristal.Pick();
-                    playerAudioManager.PlayTakeSound();
+
+        rb.MovePosition(new Vector3(horizontalMovement, verticalMovement, 0) * (speed * Time.deltaTime) +
+                        transform.position);
+        playerAudioManager.PlayStepSound();
+        switch (horizontalMovement) {
+            case -1:
+                _spriteRenderer.sprite = sprites[2];
+                _spriteRenderer.flipX = true;
+                break;
+            case 1:
+                _spriteRenderer.sprite = sprites[2];
+                _spriteRenderer.flipX = false;
+                break;
+            default:
+                switch (verticalMovement) {
+                    case -1:
+                        _spriteRenderer.sprite = sprites[1];
+                        _spriteRenderer.flipX = false;
+                        break;
+                    case 1:
+                        _spriteRenderer.sprite = sprites[0];
+                        _spriteRenderer.flipX = false;
+                        break;
+                    default:
+                        break;
                 }
-            }
-           
+
+                break;
         }
     }
-
 }
