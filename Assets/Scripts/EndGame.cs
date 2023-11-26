189ff841a8cf2fee349d52c2a2d7ead189f95e20
juauke <juauke@live.fr>
2023-11-26 09:25:34 +0100

    Fixed EndGame script

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -3,7 +3,5 @@ using UnityEngine;
 public class EndGame : MonoBehaviour {
     [SerializeField] private GameObject player;
 
-    private void OnTrigger2DEnter(Collider2D other) {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
-    }
+    private void OnTriggerEnter2D(Collider2D other) { UnityEngine.SceneManagement.SceneManager.LoadScene(2); }
 }
