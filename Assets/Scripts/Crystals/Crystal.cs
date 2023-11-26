4a0a71a7640a2374bb3beadc0ce69d1e1288ce9b
juauke <juauke@live.fr>
2023-11-26 09:21:42 +0100

    Removed typos from files regarding crYstals

---
+++ Assets/Scripts/Crystals/Crystal.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Crystal : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+
+
+    public enum Element
+    {
+        Ice,
+        Fire,
+        Elec
+    };
+
+    [SerializeField] protected TileMapManager tileMapManager;
+
+
+    public Element type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Place(Vector3 position)
+    {
+        Vector3Int tilePosition = tileMapManager.tileMap.WorldToCell(position);
+        Vector3 cristalPosition = tileMapManager.tileMap.GetCellCenterLocal(tilePosition);
+        transform.position = cristalPosition;
+        tileMapManager.getData(tilePosition).cristalIsPresent = true;
+        gameObject.SetActive(true);
+        UpdateTiles(-1);
+
+    }
+
+    public void Pick()
+    {
+        gameObject.SetActive(false);
+        UpdateTiles(1);
+    }
+
+    public virtual void UpdateTiles(int factor){}
+    
+}
---
+++ Assets/Scripts/Crystals/IceCrystal.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class IceCrystal : Crystal
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        type = Element.Ice;
+    }
+
+    void Start()
+    {
+        UpdateTiles(-1);
+    }
+
+
+    public override void UpdateTiles(int factor)
+    {
+        Vector3Int tilePosition = tileMapManager.tileMap.WorldToCell(transform.position);
+        int x = tilePosition.x;
+        int y = tilePosition.y;
+        for (int i = -2; i <= 2; i++)
+        {
+            for (int j = -2; j <= 2; j++)
+            {
+                if (math.abs(i * j) < 4)
+                {
+                    var tileToUpdatePosition = new Vector3Int(x + i, y + j, 0);
+                    Tile tileToUpdate = tileMapManager.tileMap.GetTile<Tile>(tileToUpdatePosition);
+                    
+                        tileMapManager.getData(tileToUpdatePosition).temperature += factor;
+                    
+                    tileMapManager.UpdateTile(tileMapManager.getData(tileToUpdatePosition));
+                    
+                }
+            }
+        }
+    }
+}
